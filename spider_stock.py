454cea444988d0344426dab6d27e5721a3309ad7
Jiazheng Zhou <zhoujiazheng@sunseaaiot.com>
2018-12-20 10:58:42 +0800

    new(new_script)

---
+++ spider_stock.py
@@ -0,0 +1,67 @@
+import threading
+import time
+import logging
+import random
+import Queue
+import requests
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-9s) %(message)s', )
+
+BUF_SIZE = 10
+q = Queue.Queue(BUF_SIZE)
+
+
+def get_stock(code):
+    url = 'http://hq.sinajs.cn/list=' + code
+    resp = requests.get(url)
+    print('%s\n' % resp.text)
+    return resp.text
+
+
+
+class ProducerThread(threading.Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, verbose=None):
+        super(ProducerThread, self).__init__()
+        self.target = target
+        self.name = name
+
+    def run(self):
+        while True:
+            if not q.full():
+                # item = random.randint(1, 10)
+                item = get_stock('sz000878')
+                q.put(item)
+                logging.debug('Putting ' + item
+                              + ' : ' + str(q.qsize()) + ' items in queue')
+                time.sleep(random.random())
+        return
+
+
+class ConsumerThread(threading.Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, verbose=None):
+        super(ConsumerThread, self).__init__()
+        self.target = target
+        self.name = name
+        return
+
+    def run(self):
+        while True:
+            if not q.empty():
+                item = q.get()
+                logging.debug('Getting ' + item
+                              + ' : ' + str(q.qsize()) + ' items in queue')
+                time.sleep(random.random())
+        return
+
+
+if __name__ == '__main__':
+    p = ProducerThread(name='producer')
+    c = ConsumerThread(name='consumer')
+
+    p.start()
+    time.sleep(2)
+    c.start()
+    time.sleep(2)
